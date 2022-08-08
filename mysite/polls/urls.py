075fa200035ff7be966eae22d53933346f2bb1ab
Omar Magdy <24939461+OmarThinks@users.noreply.github.com>
2022-08-08 04:42:42 +0200

    ResultView

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -8,17 +8,23 @@ urlpatterns = [
     
     
     # ex: /polls/
-    path("", views.QuestionIndexView.as_view(), name="index"),
-    
-    
+    path("", views.IndexView.as_view(), name="index"),
+    # ex: /polls/5/
+    path("<int:pk>/", views.DetailView.as_view(), name="detail"),
+    # ex: /polls/5/results/
+    path("<int:pk>/results/", views.ResultsView.as_view(), name="results"),
+    # ex: /polls/5/vote/
+    path('<int:question_id>/vote/', views.vote, name='vote'),
+
+
     # ex: /polls/old/
     path('old/', views.index_old, name='index_old'),
     # ex: /polls/old/5/
-    path('old/<int:question_id>/', views.detail_old, name='detail'),
-    # ex: /polls/5/results/
-    path('<int:question_id>/results/', views.results, name='results'),
-    # ex: /polls/5/vote/
-    path('<int:question_id>/vote/', views.vote, name='vote'),
+    path('old/<int:question_id>/', views.detail_old, name='detail_old'),
+    # ex: /polls/old/5/results/
+    path('old/<int:question_id>/results/', views.results, name='results_old'),
+    
+
 ]
 
 
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -9,8 +9,8 @@ from django.views import generic
 from pprint import pprint as pp
 
 
-class QuestionIndexView(generic.ListView):
-    model = Question
+class IndexView(generic.ListView):
+    #model = Question
     template_name = "polls/index.html"
     context_object_name = 'latest_question_list'
     
@@ -18,6 +18,14 @@ class QuestionIndexView(generic.ListView):
         """Return the last five published questions."""
         return Question.objects.order_by('-pub_date')[:5]
 
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
 
 
 
